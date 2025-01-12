cab49a91a73b8d73220d907393acb4bd220de603
Matteo Conti <matteo.conti7@gmail.com>
2025-01-12 02:36:25 +0100

    First commit

---
+++ CursorApp/Program.cs
@@ -0,0 +1,122 @@
+﻿using System.Text.Json;
+using Windows.Win32;
+using Windows.Win32.UI.WindowsAndMessaging;
+
+if (CheckForCustom())
+{
+    SetDefaultCursor();
+}
+else
+{
+    var parts = ReadCursorPartsFromJson();
+    SetCustomCursor(parts);
+}
+
+void SetCustomCursor(CursorParts parts)
+{
+    // NormalSelect
+    var c = PInvoke.LoadImage(null, Path.Combine(Directory.GetCurrentDirectory(), "Cursor", parts.NormalSelect), GDI_IMAGE_TYPE.IMAGE_CURSOR, 0, 0, IMAGE_FLAGS.LR_LOADFROMFILE);
+    PInvoke.SetSystemCursor(c, SYSTEM_CURSOR_ID.OCR_NORMAL);
+    // TextSelect
+    c = PInvoke.LoadImage(null, Path.Combine(Directory.GetCurrentDirectory(), "Cursor", parts.TextSelect), GDI_IMAGE_TYPE.IMAGE_CURSOR, 0, 0, IMAGE_FLAGS.LR_LOADFROMFILE);
+    PInvoke.SetSystemCursor(c, SYSTEM_CURSOR_ID.OCR_IBEAM);
+    // BusySelect
+    c = PInvoke.LoadImage(null, Path.Combine(Directory.GetCurrentDirectory(), "Cursor", parts.BusySelect), GDI_IMAGE_TYPE.IMAGE_CURSOR, 0, 0, IMAGE_FLAGS.LR_LOADFROMFILE);
+    PInvoke.SetSystemCursor(c, SYSTEM_CURSOR_ID.OCR_WAIT);
+    // PrecisionSelect
+    c = PInvoke.LoadImage(null, Path.Combine(Directory.GetCurrentDirectory(), "Cursor", parts.PrecisionSelect), GDI_IMAGE_TYPE.IMAGE_CURSOR, 0, 0, IMAGE_FLAGS.LR_LOADFROMFILE);
+    PInvoke.SetSystemCursor(c, SYSTEM_CURSOR_ID.OCR_CROSS);
+    // AlternateSelect
+    c = PInvoke.LoadImage(null, Path.Combine(Directory.GetCurrentDirectory(), "Cursor", parts.AlternateSelect), GDI_IMAGE_TYPE.IMAGE_CURSOR, 0, 0, IMAGE_FLAGS.LR_LOADFROMFILE);
+    PInvoke.SetSystemCursor(c, SYSTEM_CURSOR_ID.OCR_UP);
+    // DiagonalResize1
+    c = PInvoke.LoadImage(null, Path.Combine(Directory.GetCurrentDirectory(), "Cursor", parts.DiagonalResize1), GDI_IMAGE_TYPE.IMAGE_CURSOR, 0, 0, IMAGE_FLAGS.LR_LOADFROMFILE);
+    PInvoke.SetSystemCursor(c, SYSTEM_CURSOR_ID.OCR_SIZENWSE);
+    // DiagonalResize2
+    c = PInvoke.LoadImage(null, Path.Combine(Directory.GetCurrentDirectory(), "Cursor", parts.DiagonalResize2), GDI_IMAGE_TYPE.IMAGE_CURSOR, 0, 0, IMAGE_FLAGS.LR_LOADFROMFILE);
+    PInvoke.SetSystemCursor(c, SYSTEM_CURSOR_ID.OCR_SIZENESW);
+    // HorizontalResize
+    c = PInvoke.LoadImage(null, Path.Combine(Directory.GetCurrentDirectory(), "Cursor", parts.HorizontalResize), GDI_IMAGE_TYPE.IMAGE_CURSOR, 0, 0, IMAGE_FLAGS.LR_LOADFROMFILE);
+    PInvoke.SetSystemCursor(c, SYSTEM_CURSOR_ID.OCR_SIZEWE);
+    // VerticalResize
+    c = PInvoke.LoadImage(null, Path.Combine(Directory.GetCurrentDirectory(), "Cursor", parts.VerticalResize), GDI_IMAGE_TYPE.IMAGE_CURSOR, 0, 0, IMAGE_FLAGS.LR_LOADFROMFILE);
+    PInvoke.SetSystemCursor(c, SYSTEM_CURSOR_ID.OCR_SIZENS);
+    // Move
+    c = PInvoke.LoadImage(null, Path.Combine(Directory.GetCurrentDirectory(), "Cursor", parts.Move), GDI_IMAGE_TYPE.IMAGE_CURSOR, 0, 0, IMAGE_FLAGS.LR_LOADFROMFILE);
+    PInvoke.SetSystemCursor(c, SYSTEM_CURSOR_ID.OCR_SIZEALL);
+    // Unavailable
+    c = PInvoke.LoadImage(null, Path.Combine(Directory.GetCurrentDirectory(), "Cursor", parts.Unavailable), GDI_IMAGE_TYPE.IMAGE_CURSOR, 0, 0, IMAGE_FLAGS.LR_LOADFROMFILE);
+    PInvoke.SetSystemCursor(c, SYSTEM_CURSOR_ID.OCR_NO);
+    // LinkSelect
+    c = PInvoke.LoadImage(null, Path.Combine(Directory.GetCurrentDirectory(), "Cursor", parts.LinkSelect), GDI_IMAGE_TYPE.IMAGE_CURSOR, 0, 0, IMAGE_FLAGS.LR_LOADFROMFILE);
+    PInvoke.SetSystemCursor(c, SYSTEM_CURSOR_ID.OCR_HAND);
+    // WorkingInBackground
+    c = PInvoke.LoadImage(null, Path.Combine(Directory.GetCurrentDirectory(), "Cursor", parts.WorkingInBackground), GDI_IMAGE_TYPE.IMAGE_CURSOR, 0, 0, IMAGE_FLAGS.LR_LOADFROMFILE);
+    PInvoke.SetSystemCursor(c, SYSTEM_CURSOR_ID.OCR_APPSTARTING);
+}
+
+void SetDefaultCursor()
+{
+    unsafe
+    {
+        PInvoke.SystemParametersInfo(SYSTEM_PARAMETERS_INFO_ACTION.SPI_SETCURSORS, 0, null, SYSTEM_PARAMETERS_INFO_UPDATE_FLAGS.SPIF_SENDCHANGE);
+    }
+}
+
+static bool CheckForCustom()
+{
+    const string fileName = "IsCustom";
+    try
+    {
+        if (File.Exists(fileName))
+        {
+            File.Delete(fileName);
+            return true;
+        }
+
+        using FileStream fs = File.Create(fileName);
+        return false;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"An error occurred: {ex.Message}");
+    }
+    return false;
+}
+
+static CursorParts ReadCursorPartsFromJson()
+{
+    var filename = "SetCursorParts.json";
+    try
+    {
+        // Read the JSON string from the file
+        string jsonString = File.ReadAllText(filename);
+
+        // Deserialize the JSON string to the specified type
+        CursorParts data = JsonSerializer.Deserialize<CursorParts>(jsonString) ?? new CursorParts();
+
+        return data;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"An error occurred while reading JSON: {ex.Message}");
+        throw;
+    }
+}
+
+public class CursorParts
+{
+    public string NormalSelect { get; set; } = string.Empty;
+    public string TextSelect { get; set; } = string.Empty;
+    public string BusySelect { get; set; } = string.Empty;
+    public string PrecisionSelect { get; set; } = string.Empty;
+    public string AlternateSelect { get; set; } = string.Empty;
+    public string DiagonalResize1 { get; set; } = string.Empty;
+    public string DiagonalResize2 { get; set; } = string.Empty;
+    public string HorizontalResize { get; set; } = string.Empty;
+    public string VerticalResize { get; set; } = string.Empty;
+    public string Move { get; set; } = string.Empty;
+    public string Unavailable { get; set; } = string.Empty;
+    public string LinkSelect { get; set; } = string.Empty;
+    public string WorkingInBackground { get; set; } = string.Empty;
+}
